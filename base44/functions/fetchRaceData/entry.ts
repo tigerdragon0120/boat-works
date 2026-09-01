cc1a0991eeb42bd9889abbf290c7c3d8571dbf38
だわだっちょ <tiger.dragon.0120@gmail.com>
2026-09-01 00:04:16 +0000

    External agent changes

--- base44/functions/fetchRaceData/entry.ts
+++ base44/functions/fetchRaceData/entry.ts
@@ -193,7 +193,12 @@ export default async function(req) {
     if (existing.length > 0) {
       race = await base44.asServiceRole.entities.Race.update(existing[0].id, raceData);
     } else {
-      race = await base44.asServiceRole.entities.Race.create(raceData);
+      // 並行ワーカー対策：create直前に同一Raceを再確認
+      const dup = await base44.asServiceRole.entities.Race.filter({
+        race_date: raceDate, venue_code: jcd, race_number: raceNumber, data_source: "official"
+      }, "-updated_date", 5).catch(() => []);
+      if (dup.length > 0) race = await base44.asServiceRole.entities.Race.update(dup[0].id, raceData);
+      else race = await base44.asServiceRole.entities.Race.create(raceData);
     }
 
     // RaceEntry 再保存。
